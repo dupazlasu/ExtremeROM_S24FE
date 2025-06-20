c9672fd2cfbcd4b14db3b4235b8c5363aee6b36c
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-06-20 19:13:06 +0300

    fix hevc encoding with s25u hexpatched lib
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- platform/exynos9820/patches/camera/customize.sh
+++ platform/exynos9820/patches/camera/customize.sh
@@ -29,7 +29,7 @@ do
 done
 
 # Patch libstagefright.so to remove HDR10+ check
-HEX_PATCH "$WORK_DIR/system/system/lib64/libstagefright.so" "010140f9dd1f0594a04c0034" "010140f91f2003d51f2003d5"
+#HEX_PATCH "$WORK_DIR/system/system/lib64/libstagefright.so" "010140f9dd1f0594a04c0034" "010140f91f2003d51f2003d5"
 
 # Add prebuilt libs from other devices
 BLOBS_LIST="
--- platform/exynos990/patches/camera/customize.sh
+++ platform/exynos990/patches/camera/customize.sh
@@ -51,7 +51,7 @@ fi
 patchelf --add-needed "libc++_shared.so" "$WORK_DIR/system/system/lib64/libMultiFrameProcessing20Core.camera.samsung.so"
 
 # Patch libstagefright.so to remove HDR10+ check
-HEX_PATCH "$WORK_DIR/system/system/lib64/libstagefright.so" "010140f9dd1f0594a04c0034" "010140f91f2003d51f2003d5"
+#HEX_PATCH "$WORK_DIR/system/system/lib64/libstagefright.so" "010140f9dd1f0594a04c0034" "010140f91f2003d51f2003d5"
 
 # Add prebuilt libs from other devices
 BLOBS_LIST="
